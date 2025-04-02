578c7bd33a5bf2fd9062b207637865facc43f75a
pavel <steam7339090@gmail.com>
2025-04-02 07:41:42 +0000

    Second commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'pug')
 
 
 app.get("/", async (_req, res) => {
-   res.render('index', {message: "Hello World!"})
+   res.render('index', {message: "Hello World! hello hell"})
 });
 
 
